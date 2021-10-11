01695e538fb1f221399fe9da4aa0184909219e80
Adam Oellermann <adam@oellermann.com>
2021-10-11 12:00:59 +0100

    Added player editing

--- Software/Launcher/launcher.py
+++ Software/Launcher/launcher.py
@@ -55,7 +55,7 @@ class Team:
     def __init__(self) :
         self.name = ""
         self.colour = ""
-        self.teamid = unique_id()
+        self.teamid = unique_id() # internal identifier
         self.teamnumber = "" # IR code
 
 class Player:
@@ -63,9 +63,17 @@ class Player:
         self.name = ""
         self.colour = ""
         self.team = None
-        self.playerid = ""
+        self.playerid = unique_id() # internal identifier
         self.gunid = ""
         self.playernumber = "" # IR code
+    
+    def summary(self):
+        if self.team != None:
+            affiliation = "Team: [{0}] Colour: [{1}]".format(self.team.name, self.team.colour)
+        else:
+            affiliation = "Colour: [{0}]".format(self.colour)
+        summary = "Name: [{0}] {1} Gun: [{2}]".format(self.name, affiliation, self.gunid)
+        return summary
 
 game = Game()
 teams = {}
@@ -80,13 +88,21 @@ def summary():
 
     if game_ready:
         print("Game Name: [{0}] Type: [{1}] Time: [{2}]".format(game.name, game.type, game.maxtime))
+    else:
+        print("Game:")
     if teams_ready:
         print("Teams:")
         for key, team in teams.items():
             print("-- Team: [{0}] Colour: [{1}] ID: [{2}]".format(team.name, team.colour, key))
+    else:
+        print("Teams:")
     if players_ready:
-        print("TODO Players")
-    
+        print("Players:")
+        for key, player in players.items():            
+            print("--" + player.summary())
+    else:
+        print("Players:")
+        
 
 def get_value(prompt, default):
     val = input(prompt)
@@ -119,7 +135,7 @@ def pick_player():
     playerno = 1
     for key, player in players.items():
         lookup[playerno] = player
-        print("{0}: {1} - {2}".format(playerno, player.name, player.team.name))
+        print("{0}: {1}".format(playerno, player.summary()))
         playerno += 1
     
     print()
@@ -129,39 +145,69 @@ def pick_player():
     else:
         return None
 
+def player_pick_team():
+    # prompt to select a team, and then return it
+    global game, teams, players
+    
+    print("Team:")
+    lookup = {}
+    teamno = 1
+    for key, team in teams.items():
+        lookup[teamno] = team
+        print("    {0}: {1} - {2}".format(teamno, team.name, team.colour))
+        teamno += 1
+    
+    print()
+    choice = int(input("Select a team (0 for no team): > "))
+    if (choice in lookup):
+        return lookup[choice]
+    else:
+        return None
+
 def edit_player_values(player):
-    pass 
+    # actually edit the properties of a given player
+    # TODO: validate inputs
+    # TODO: when input a Gun ID, send it a 'ping' msg, and track 
+    player.name = get_value("Name [{0}] > ".format(player.name), player.name)
+    player.team = player_pick_team()
+    if player.team == None:
+        cols = ""
+        for c in colours:
+            cols += c + " "
+        print("Available Colours: [{0}]".format(cols))
+        player.colour = get_value("Colour [{0}] > ".format(player.colour), player.colour)
+    player.gunid = get_value("Gun ID [{0}] > ".format(player.gunid), player.gunid)
 
-def delete_player(): # TODO
-    # add a new team
-    global teams
+def delete_player():
+    # delete a player
+    global players
     
     cls()
-    print("Delete Team")
+    print("Delete Player")
     print("--------------------------------------------------------------------------------")
-    del_team = pick_team()
-    del teams[del_team.teamid]
+    del_player = pick_player()
+    del players[del_player.playerid]
 
-def edit_player(): # TODO
-    # add a new team
-    global teams
+def edit_player():
+    # edit a player
+    global players
     
     cls()
-    print("Edit Team")
+    print("Edit Player")
     print("--------------------------------------------------------------------------------")
-    edit_team = pick_team()
-    edit_team_values(edit_team)
+    edit_player = pick_player()
+    edit_player_values(edit_player)
 
-def add_player(): # TODO
+def add_player(): 
     # add a new player
-    global teams
+    global players
     
     cls()
-    print("Add New Team")
+    print("Add New Player")
     print("--------------------------------------------------------------------------------")
-    new_team = Team()
-    edit_team_values(new_team)
-    teams[new_team.teamid] = new_team
+    new_player = Player()
+    edit_player_values(new_player)
+    players[new_player.playerid] = new_player
 
 def pick_team():
     # prompt to select a team, and then return it
@@ -182,6 +228,8 @@ def pick_team():
         return None
 
 def edit_team_values(team):
+    # actually edit the properties of a given team
+    # TODO: validate input: colour
     team.name = get_value("Name [{0}] > ".format(team.name), team.name)
     # TODO colour enumeration
     cols = ""
@@ -192,7 +240,7 @@ def edit_team_values(team):
 
 
 def delete_team():
-    # add a new team
+    # delete a team
     global teams
     
     cls()
@@ -202,7 +250,7 @@ def delete_team():
     del teams[del_team.teamid]
 
 def edit_team():
-    # add a new team
+    # edit a team
     global teams
     
     cls()
@@ -223,6 +271,7 @@ def add_team():
     teams[new_team.teamid] = new_team
 
 def get_teams():
+    # team setup menu
     global game_ready, teams_ready, players_ready, launched
     global game, teams, players
 
@@ -252,13 +301,39 @@ def get_teams():
 
 
 
-    print("get_teams")
     teams_ready = True
 
 def get_players():
+    # player setup menu
     global game_ready, teams_ready, players_ready, launched
     global game, teams, players
-    print("get_players")
+
+    players_finished = False
+    while not players_finished:
+        cls()
+        print("--------------------------------------------------------------------------------")
+        print("Player Setup")
+        print("--------------------------------------------------------------------------------")
+        print("Current Players:")
+        for key, player in players.items():
+            print(player.summary())
+        print("--------------------------------------------------------------------------------")
+        print("Options:")
+        print()
+        print("1. Add Player")
+        print("2. Edit Player")
+        print("3. Delete Player")
+        print("4. Return to Main Menu")
+
+        option = input("Select an option: > ")
+        if option == "1": add_player()
+        if option == "2": edit_player()
+        if option == "3": delete_player()
+        if option == "4": players_finished = True
+
+
+
+
     players_ready = True
 
 def launch_game():
@@ -307,11 +382,11 @@ def menu():
         launch['3'] = get_players
         menu.append(edit_players)
 
-        if game_ready and teams_ready and players_ready:
+        if game_ready and players_ready: # removed teams_ready for team-optional games
             launch_game_text = '4. Launch Game'
             launch['4'] = launch_game
         else:
-            launch_game_text = 'You cannot launch the game until all TODOs are resolved.'
+            launch_game_text = 'You cannot launch until Game Parameters and Players have been configured.'
         menu.append(launch_game_text)
         
         for option in menu:
